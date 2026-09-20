e545ccfbeb8ce18871a2c5080ce2e6611a8b9af8
Heber Pineda <157323289+Heber-Pineda@users.noreply.github.com>
2026-09-20 14:55:12 -0600

    Agrega Swagger para documentar y probar la API

--- src/ClinData.API/Program.cs
+++ src/ClinData.API/Program.cs
@@ -22,6 +22,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "ClinData API v1");
+    });
 }
 
 app.UseHttpsRedirection();
